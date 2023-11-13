d70fa0823d5f3becd6b1e597ce30a777fda131cb
marlisonmourao <bentesmourao@gmail.com>
2023-11-12 21:51:06 -0400

    ✨ feat:
    getters & setters entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,9 +3,11 @@ import { Optional } from '@/core/types/optional'
 import { Slug } from './value-objects/slug/slug'
 import { UniqueEntityId } from './value-objects/unique-entity-id/unique-entity-id'
 
+import dayjs from 'dayjs'
+
 interface QuestionProps {
   authorId: UniqueEntityId
-  besAnswerId?: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
   content: string
   slug: Slug
@@ -14,14 +16,71 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 200).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
   static create(
-    props: Optional<QuestionProps, 'createdAt'>,
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
     id?: UniqueEntityId,
   ) {
     const question = new Question(
       {
         ...props,
         createdAt: new Date(),
+        slug: props.slug ?? Slug.createFromText(props.title),
       },
       id,
     )
